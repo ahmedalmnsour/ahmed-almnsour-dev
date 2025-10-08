e58ba5aa259343b458fc342b27e6192d78dc1f88
almnsour <almnsour.ahmed@hotmail.com>
2025-10-08 21:26:59 +0300

    HeroSection Update

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import styles from './HeroSection.module.css';
 
+// استيراد الكلاس الخاص بمكتبة الأيقونات
+// هذا السطر ليس ضروريا إذا قمت بإضافة الرابط في index.html, ولكنه يساعد على الوضوح
+// import 'material-symbols'; 
+
 export default function HeroSection() {
   return (
-    // لاحظ كيف أن لدينا الآن div واحد فقط وهو الذي يحمل الـ id والكلاس معًا
     <div id="home" className={styles.heroContainer}>
       <h1 className={styles.mainTitle}>أحمد المنصور</h1>
-      <p className={styles.subtitle}>مُطوِّر برمجيات</p>
-      <p className={styles.slogan}><strong> أُحَوِّل القهوةَ إلى كود ☕</strong></p>
+      <p className={styles.subtitle}>مُطوِّر برمجيات</p>
+      <p className={styles.slogan}>
+        <strong>
+          أُحَوِّل القهوةَ ☕ إلى كود 
+          <span className={`material-symbols-outlined ${styles.codeIcon}`}>
+            code
+          </span>
+        </strong>
+      </p>
     </div>
   );
 }
