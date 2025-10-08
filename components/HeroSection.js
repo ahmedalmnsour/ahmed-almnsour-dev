df84f02c2065c1fc7f35a99373761978c15ed1e9
almnsour <almnsour.ahmed@hotmail.com>
2025-10-08 20:36:45 +0300

    HeroSection Update

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -6,8 +6,8 @@ export default function HeroSection() {
     // لاحظ كيف أن لدينا الآن div واحد فقط وهو الذي يحمل الـ id والكلاس معًا
     <div id="home" className={styles.heroContainer}>
       <h1 className={styles.mainTitle}>أحمد المنصور</h1>
-      <p className={styles.subtitle}>مطور برمجيات</p>
-      <p className={styles.slogan}><strong>أنا أحول القهوة إلى كود ☕</strong></p>
+      <p className={styles.subtitle}>مُطوِّر برمجيات</p>
+      <p className={styles.slogan}><strong> أُحَوِّل القهوةَ إلى كود ☕</strong></p>
     </div>
   );
 }
