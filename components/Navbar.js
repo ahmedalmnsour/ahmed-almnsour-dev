6f3162a66b1a89ae2b27aef263bbafec85c8cb16
almnsour <almnsour.ahmed@hotmail.com>
2025-10-10 12:58:47 +0300

    fix(Navbar): Correct routing for multi-page navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,27 +2,28 @@
 
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const pathname = usePathname();
 
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-  const handleLinkClick = () => setIsMenuOpen(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-  const projectsLinkClassName = `${styles.navLink} ${pathname === '/projects' ? styles.activeLink : ''}`;
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
     <header className={styles.navbar}>
       <Link href="/#home" className={styles.logo} onClick={handleLinkClick}>
         Ahmed.Almnsour.dev
       </Link>
-
+      
       <nav>
         <ul className={isMenuOpen ? `${styles.navLinks} ${styles.active}` : styles.navLinks}>
-
+          
           <li className={styles.navLink}><Link href="/#home" onClick={handleLinkClick}>الرئيسية</Link></li>
           <li className={styles.navLink}><Link href="/#about" onClick={handleLinkClick}>نبذة عني</Link></li>
           <li className={styles.navLink}><Link href="/#project" onClick={handleLinkClick}>أعمالي</Link></li>
