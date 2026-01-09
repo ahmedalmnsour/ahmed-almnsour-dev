dea4bad21c0e45ef7c9db228598dc7877eb54492
almnsour <almnsour.ahmed@gmail.com>
2026-01-09 11:12:06 +0300

    perf: optimize LCP with image priority and dynamic imports

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import HeroSection from '@/components/HeroSection';
-import AboutSection from '@/components/AboutSection';
-import FeaturedProject from '@/components/FeaturedProject';
-import SkillsSection from '@/components/SkillsSection';
-import FeaturedArticle from '@/components/FeaturedArticle';
-import ContactSection from '@/components/ContactSection';
+
+const AboutSection = dynamic(() => import('@/components/AboutSection'));
+const FeaturedProject = dynamic(() => import('@/components/FeaturedProject'));
+const SkillsSection = dynamic(() => import('@/components/SkillsSection'));
+const FeaturedArticle = dynamic(() => import('@/components/FeaturedArticle'));
+const ContactSection = dynamic(() => import('@/components/ContactSection'));
 
 export default function Home() {
-
   return (
     <>
       <HeroSection />
